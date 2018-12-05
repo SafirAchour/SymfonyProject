87f6766eacaba3236c38d4b8e545b46e35b2605f
jonatuxdr <jonathan.deriemaecker@gmail.com>
2018-12-05 16:35:35 +0100

    Symfony project classroom

---
+++ src/Entity/User.php
@@ -0,0 +1,137 @@
+<?php
+namespace App\Entity;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="UUID")
+     * @ORM\Column(type="guid")
+     */
+    private $id;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $salt;
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $username;
+    /**
+     * @ORM\ManyToMany(targetEntity="Role")
+     * @ORM\JoinTable(
+     *      name="users_roles",
+     *      joinColumns={
+     *          @ORM\JoinColumn(
+     *              name="user_id",
+     *              referencedColumnName="id"
+     *          )
+     *      },
+     *      inverseJoinColumns={
+     *          @ORM\JoinColumn(
+     *              name="role_id",
+     *              referencedColumnName="id"
+     *          )
+     *      }
+     * )
+     */
+    private $roles;
+    
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function __construct()
+    {
+        $this->roles = new ArrayCollection();
+    }
+    
+    
+    
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    public function eraseCredentials()
+    {}
+
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+    
+    public function getRoles() 
+    {
+        $roles = [];
+        foreach ($this->roles as $role)
+        {
+            $roles[] = $role->getLabel();
+        }
+        return $roles;
+    }
+    public function setRoles($roles)
+    {
+        foreach ($roles as $role)
+        {
+            $this->addRole($role);
+        }
+        return $this;
+    }
+    
+    public function addRole(Role $role) 
+    {
+        if (!$this->roles->contains($role)) 
+        {
+            $this->roles->add($role);
+        };
+        
+        return $this;
+    }
+    
+    public function removeRole(Role $role)
+    {
+        if (!$this->roles->contains($role))
+        {
+            $this->roles->removeElement($role);
+        };
+        
+        return $this;
+    }
+    
+    public function setUsername($username) {
+        $this->username = $username;
+        
+        return $this;
+    }
+    
+    public function setPassword($password) {
+        $this->password = $password;
+        
+        return $this;
+    }
+    
+    public function setSalt($salt) {
+        $this->salt = $salt;
+        
+        return $this;
+    }
+    //EquatableInterface -> check it because the $_SESSION will store not sensitive information and it will eraseCredentials and when the user is redirected after the sign up form there will be a missmatch !!!!
+}
